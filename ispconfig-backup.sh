da1121f7980116b0c9bb6eb1fde48534493bf02c
Richard Reijmers <richard@osshosting.nl>
2013-06-25 22:57:07 +0200

    Some script layout in comments

--- ispconfig-backup.sh
+++ ispconfig-backup.sh
@@ -1,1 +1,41 @@
 #!/bin/bash
+
+
+# Required Variables
+aREQUIRED=( --run )
+
+# Optional Variables
+aOPTIONAL=( --verbose --dryrun)
+
+# This will show when parameter --help is given
+EXPLANATION="
+*** REQUIRED ***
+--run			- y/n	- 	a failsafe
+*** OPTIONAL ***              
+--verbose	- y/n		-	Show commands while executing
+--dryrun	- y/n		-	Just show list of commands, do nothing
+"
+
+# Include
+source ../loop-parameters/include.sh
+
+# Required call
+LOOPPARAMETERS $@
+
+
+# Get list of domains from /var/www
+
+# Loop domain list and create .tar.gz files for each
+
+# Get list of databases from /var/lib/mysql
+
+# Loop database list an create .sql.gz files for each
+
+# Echo parameter 1
+command="echo ${run}"
+SHOWIFVERBOSE "${command}"	
+DRYRUNOREXECUTE ${command}
+
+
+
+
