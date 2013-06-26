3326ae3efd575426b741e784d5275d3b65b848d9
Richard Reijmers <richard@osshosting.nl>
2013-06-26 21:32:30 +0200

    loop domains test

--- ispconfig-backup.sh
+++ ispconfig-backup.sh
@@ -12,8 +12,10 @@ EXPLANATION="
 *** REQUIRED ***
 --run			- y/n	- 	a failsafe
 *** OPTIONAL ***              
---verbose	- y/n		-	Show commands while executing
---dryrun	- y/n		-	Just show list of commands, do nothing
+--retention		- integer	-	how many days retention of backups to keep, default = 1
+--directory		- path		-	If omitted /var/backup (not backups) will be used
+--verbose		- y/n		-	Show commands while executing
+--dryrun		- y/n		-	Just show list of commands, do nothing
 "
 
 # Include
@@ -24,18 +26,18 @@ LOOPPARAMETERS $@
 
 
 # Get list of domains from /var/www
+domainlist=`find /var/www/ -type l -maxdepth 1`
 
 # Loop domain list and create .tar.gz files for each
+for domain in ${domainslist[*]}
+do
+	command="tar cvfz ${backuplocation}/${domain:9}.tar.gz ${domain}"
+	SHOWIFVERBOSE "${command}"
+	DRYRUNOREXECUTE ${command}
+done
 
 # Get list of databases from /var/lib/mysql
 
 # Loop database list an create .sql.gz files for each
 
-# Echo parameter 1
-command="echo ${run}"
-SHOWIFVERBOSE "${command}"	
-DRYRUNOREXECUTE ${command}
-
-
-
-
+# Delete older backups
